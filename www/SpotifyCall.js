9401ee948f9f92ec53bde35aef0d981b7577a464
mathiasKjeldsen <Mathias@mekbas.dk>
2020-01-13 07:43:32 +0100

    Update SpotifyCall.js

--- www/SpotifyCall.js
+++ www/SpotifyCall.js
@@ -19,29 +19,90 @@ SpotifyCall.initialize = function(onSuccess, onError, options) {
     exec(onSuccess, onError, "SpotifyCall", "initialize", [options]);
 };
 
-SpotifyCall.doConnect = function(onSuccess, onError, token, playuri) {
-    exec(onSuccess, onError, "SpotifyCall", "doConnect", [token, playuri]);
+/**
+ * If a session has been initiated and the user requests to play a track - the Spotify app must be woken using authorizeAndPlay.
+ * This function wakes up the Spotify app and starts playing the requests URI. 
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. Event is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ * 
+ * @param {string} accessToken AccessToken returned by initialize function.
+ * @param {string} playURI URI of the requested track.
+ */
+
+SpotifyCall.doConnect = function(onSuccess, onError, token, playURI) {
+    exec(onSuccess, onError, "SpotifyCall", "doConnect", [token, playURI]);
 };
+
+/**
+ * Function that returns the accessToken for the current session.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. AccessToken is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ */
 
 SpotifyCall.getToken = function(onSuccess, onError) {
     exec(onSuccess, onError, "SpotifyCall", "getToken", []);
 };
 
+/**
+ * Function that starts playing a track in the Spotify app.
+ * If appRemote is not connected - this will happen first.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. Event is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ * @param {string} playURI URI of the requested track.
+ */
+
 SpotifyCall.playURI = function(onSuccess, onError, playuri) {
     exec(onSuccess, onError, "SpotifyCall", "playURI", [playuri]);
 };
+
+/**
+ * Function that pauses the currently playing track in the Spotify app.
+ * If appRemote is not connected - this will happen first.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. Event is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ */
 
 SpotifyCall.pause = function(onSuccess, onError) {
     exec(onSuccess, onError, "SpotifyCall", "pause", []);
 };
 
+/**
+ * Function that resumes the currently loaded track in the Spotify app.
+ * If appRemote is not connected - this will happen first.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. Event is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ */
+
 SpotifyCall.resume = function(onSuccess, onError) {
     exec(onSuccess, onError, "SpotifyCall", "resume", []);
 };
 
-SpotifyCall.seek = function(onSuccess, onError, time) {
-    exec(onSuccess, onError, "SpotifyCall", "seek", [time]);
+/**
+ * Function that seeks to the provided position of the currently playing track in the Spotify app.
+ * If appRemote is not connected - this will happen first.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. Event is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ * @param {number} position Target position in milliseconds
+ */
+
+SpotifyCall.seek = function(onSuccess, onError, position) {
+    exec(onSuccess, onError, "SpotifyCall", "seek", [position]);
 };
+
+/**
+ * Function that returns the Spotify app's current playerState.
+ * If appRemote is not connected - this will happen first.
+ *
+ * @param {function} onSuccess Function to call when a session has been initialized. playerState object is returned.
+ * @param {function} onError Function to call if a session could not be initialized. Error description is returned.
+ */
+
 
 SpotifyCall.getPlayerState = function(onSuccess, onError) {
     exec(onSuccess, onError, "SpotifyCall", "getPlayerState", []);
